23e7756f470b088f54e482c7f98290034f54898c
Yudai Hayashi <yudai@wantedly.com>
2022-12-15 14:45:15 +0900

    setup ssh

--- script.sh
+++ script.sh
@@ -1,11 +1,28 @@
 #! /bin/bash
 
-apt update -y && apt install -y vim git 
+apt update -y \
+    && apt install -y vim \
+        git \
+        fzf \
+        fasd
 
 cp ./.vimrc $HOME/.vimrc
 cp ./.bashrc $HOME/.bashrc
 cp ./.bash_aliases $HOME/.bash_aliases
 
+source ./.env
+mkdir $HOME/.ssh
+
+echo "Host github.com" > $HOME/.ssh/config
+echo "  HostName github.com" >> $HOME/.ssh/config
+echo "  User git" >> $HOME/.ssh/config
+echo "  Port $GITHUB_SSH_PORT" >> $HOME/.ssh/config
+echo "  IdentityFile $GITHUB_SSH_IDENTITY" >> $HOME/.ssh/config
+echo "  IdentitiesOnly yes" >> $HOME/.ssh/config
+
 git clone https://github.com/tomasiser/vim-code-dark.git
 mkdir -p ~/.vim/colors
 mv ./vim-code-dark/colors/codedark.vim ~/.vim/colors/
+
+source ~/.bashrc
+
