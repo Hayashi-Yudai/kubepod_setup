4237dadd6d8a579d134edcb6ad11c2311ae2d80e
Yudai Hayashi <yudai@wantedly.com>
2023-01-06 18:49:11 +0900

    Fix

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-
 apt update -y \
     && apt install -y vim \
         git \
@@ -15,7 +14,8 @@ source ~/.bashrc
 source ./.env
 
 # SSH setting
-mkdir $HOME/.ssh
+mkdir -p $HOME/.ssh
+cp $PUBKEY $GITHUB_SSH_IDENTITY
 
 echo "Host github.com" > $HOME/.ssh/config
 echo "  HostName github.com" >> $HOME/.ssh/config
@@ -24,15 +24,15 @@ echo "  Port $GITHUB_SSH_PORT" >> $HOME/.ssh/config
 echo "  IdentityFile $GITHUB_SSH_IDENTITY" >> $HOME/.ssh/config
 echo "  IdentitiesOnly yes" >> $HOME/.ssh/config
 
-chmod 600 $HOME/.ssh/config
+chmod 600 $GITHUB_SSH_IDENTITY
 ssh -T git@github.com
 
 # VIM setting
 git clone https://github.com/tomasiser/vim-code-dark.git
 mkdir -p ~/.vim/colors
-mv ./vim-code-dark/colors/codedark.vim ~/.vim/colors/
+cp ./vim-code-dark/colors/codedark.vim ~/.vim/colors/
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh)"
 mkdir -p $HOME/.vim/rc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Shougo/dein-installer.vim/master/installer.sh)"
 cp ./dein.toml $HOME/.vim/rc/
 touch $HOME/.vim/rc/dein_lazy.toml
